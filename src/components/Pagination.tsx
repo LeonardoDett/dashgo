52c8100341d4b2faceadcde1e9fffe57a78d4e81
Leonardo Dettmann Krüger <leokruger2012@gmail.com>
2021-12-23 00:30:35 -0300

    modulo 3 chakra conlcuido

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,68 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+    <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          colorScheme="pink"
+          width="3"
+          disabled
+          _disabled={{
+            bgColor: 'pink.500',
+            cursor: "default"
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          colorScheme="pink"
+          width="3"
+          bgColor="gray.700"
+          _hover={{
+            bgColor: 'gray.500'
+          }}
+        >
+          2
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          colorScheme="pink"
+          width="3"
+          bgColor="gray.700"
+          _hover={{
+            bgColor: 'gray.500'
+          }}
+        >
+          3
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          colorScheme="pink"
+          width="3"
+          bgColor="gray.700"
+          _hover={{
+            bgColor: 'gray.500'
+          }}
+        >
+          4
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
