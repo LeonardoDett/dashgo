f3842dca4aa24f6e8b208b3f3c5ae79032f60f89
Leonardo Dettmann Krüger <leokruger2012@gmail.com>
2022-03-09 16:04:00 -0300

    teste

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -6,7 +6,7 @@ import { NavSection } from "./NavSection";
 export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
-      <NavSection title="GERAL">
+      <NavSection title="GERALL">
         <NavLink icon={RiDashboardLine} href="/dashboard"> Dashboard </NavLink>
         <NavLink icon={RiContactsLine} href="/users"> Usuários  </NavLink>
       </NavSection>
